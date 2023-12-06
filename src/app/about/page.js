4edcf1b6949f5c09de8e52d099a67e14f6a98426
unknown <ahnaf.ahmed.42173@gmail.com>
2023-12-06 21:22:35 +0600

    test commit

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h2>this is about page</h2>
+            h3
+        </div>
+        
+    );
+};
+
+export default AboutPage;
